c82e6f3bb41ead36692fd6d2d5aed1729d6ee997
Philipp <p.b.peter@web.de>
2025-12-09 09:17:05 +0100

    initial commit

---
+++ task/creditcalc/creditcalc.py
@@ -0,0 +1,118 @@
+import math
+import argparse
+# write your code here
+
+def print_return_input():
+    print("> ", end="")
+    return input()
+
+# print("Enter the loan principal:")
+# loan_principal = int(print_return_input())
+#
+# def number_of_month_payments():
+#     print("Enter the monthly payment:")
+#     monthly_amount = int(print_return_input())
+#     monthly_amount = math.ceil(loan_principal / monthly_amount)
+#     return monthly_amount
+#
+# def amount_per_month():
+#     print('Enter the number of months:')
+#     num_of_months = int(print_return_input())
+#     regular_amount = math.ceil(loan_principal / num_of_months)
+#     last_payment = loan_principal - (num_of_months - 1) * regular_amount
+#     print(f'Your monthly payment = {regular_amount} and the last payment = {last_payment}.')
+#
+# print("What do you want to calculate?")
+# print('type "m" - for number of monthly payments,')
+# print('type "p" - for the monthly payment:')
+#
+# # input selection of process
+# selection = print_return_input()
+# if selection == "m":
+#     monthly_payment = number_of_month_payments()
+#     print(f"It will take {monthly_payment} months to repay the loan")
+# elif selection == "p":
+#     amount_per_month()
+#     exit()
+
+def nominal_interest(i_rate):
+    return i_rate / (12 * 100)
+
+
+
+parser = argparse.ArgumentParser(prog="Loan Calculator",
+                                 description="Personal finance can be tricky, but it doesn't have to be! "
+                                             "This project helps you understand loans and mortgages with a handy "
+                                             "calculator.")
+
+parser.add_argument("--payment", nargs='?', type=float, help='The payment amount. '
+                                                             'It can be calculated using the provided principal, '
+                                                             'interest, and number of months')
+parser.add_argument("--principal", nargs='?', type=float, help='You can get its value if you know the '
+                                                               'interest, annuity payment, and number of months.')
+parser.add_argument("--periods", nargs='?', type=float, help='denotes the number of months needed to '
+                                                             'repay the loan. It\'s calculated based on the interest, '
+                                                             'annuity payment, and principal.')
+parser.add_argument("--interest", nargs='?', type=float, help='is specified without a percent sign. Note '
+                                                              'that it can accept a floating-point value. Our loan '
+                                                              'calculator can\'t calculate the interest, so it must '
+                                                              'always be provided.')
+
+args = parser.parse_args()
+# assign passed arguments
+payment = args.payment
+principal = args.principal
+periods = args.periods
+interest = args.interest
+
+ordinary_annuity = 0.0
+
+#calculate nominal interest
+if interest:
+    interest = nominal_interest(interest)
+    print('nominal interest set to:', interest)
+
+# logical selection of functions
+
+# p = payment, i = monthly interest in 12% = 0.01 , n = number of payments
+def annuity_payment(p, i, n):
+    counter = i * (1 + i) ** n
+    denominator = ((1 + i) ** n) - 1
+    temp = counter / denominator
+    return p * temp
+
+
+
+# for anyone whos struggling with the log function in example 1:
+# n = log 1 + i (A / A − i ∗ P)
+# math.log(x, base)
+# x = (A / A − i ∗ P)
+# base = 1 + i
+
+def calculate_number_of_payments(credit, rate, i_rate):
+    fraction_denominator = rate - (i_rate * credit)
+    fraction = rate / fraction_denominator
+    base = 1 + i_rate
+    return math.log(fraction, base)
+
+
+
+#annuity payment
+a = annuity_payment(principal, interest, 97.71)
+print(f'example: annuity payment is {a}')
+
+print("Number of payments")
+value = calculate_number_of_payments(principal, payment, interest)
+print(value)
+value = math.ceil(value)
+print(f"Wert aufgerundet: {value}")
+
+
+# ordinary annuity
+if principal and interest and periods and not payment:
+    value = calculate_number_of_payments()
+    print(f'value: {value}')
+elif principal and payment and interest and not periods:
+    pass
+
+
